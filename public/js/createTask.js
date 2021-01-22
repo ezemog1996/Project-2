c72e0231e6ff1a80840231a8551e14a6cc7573c3
ezemog1996 <enrique_g_gomez@hotmail.com>
2021-01-21 23:38:13 -0500

    working on createTask.js

---
+++ public/js/createTask.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+    console.log("Ready!");
+
+    const titleInput = $("#task-title");
+    const descriptionInput =("#task-description");
+    const date = ("#date");
+    const time = ("#time");
+    const points = ("#points");
+
+  });
