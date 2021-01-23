730c64fda25157a5ccc7fd52e2095aa17ea01a41
Austin Perez <austin.perez760@gmail.com>
2021-01-23 10:15:28 -0800

    required in database connection to API routes

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,20 @@
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: " ",
-    database: "striveRite_db",
+  host: "localhost",
+  user: "root",
+  password: " ",
+  database: "striveRite_db",
 });
 
-connection.connect((err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("MySQL Connected..");
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 module.exports = connection;
-
-
 
 // connection.connect(function (err) {
 //     if (err) {
