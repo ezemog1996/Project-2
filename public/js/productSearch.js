42f57af3fccbe170827c918d00b39473d5cf646d
mitchm23 <69411959+mitchm23@users.noreply.github.com>
2021-01-26 13:54:08 -0800

    fix

--- public/js/productSearch.js
+++ public/js/productSearch.js
@@ -11,8 +11,8 @@ searchButton.click(function(){
 
 
   $.ajax({
-      url: "https://api.rainforestapi.com/request?api_key=C042717A282A49FAAABB8D1663D1E7B7&amazon_domain=amazon.com&type=search&search_term="+ search,
-      type: "GET",
+  url: "https://api.rainforestapi.com/request?api_key=C042717A282A49FAAABB8D1663D1E7B7&amazon_domain=amazon.com&type=search&search_term="+ search,
+type: "GET",
       dataType: "JSON",
   }).then(function(data){
       
@@ -20,3 +20,4 @@ searchButton.click(function(){
   }).catch(function(error){
   console.log(error);
   }));
+
